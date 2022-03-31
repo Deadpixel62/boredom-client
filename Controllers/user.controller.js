f45d6d34d796d623da3523f1641a22f61d2bda16
Bouhaouche Mohammed <bouhaouche.med@hotmail.com>
2022-03-31 15:46:42 +0100

    token exp

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -104,7 +104,7 @@ userController.login = (req, res, next) => {
             });
           }
           const token = jwt.sign({ userId: user._id }, key, {
-            expiresIn: 180000,
+            expiresIn: 3,
           });
 
           res.status(200).json({
