7200478569cb52e64e1866ff8c5b251742365f7e
Bouhaouche Mohammed <bouhaouche.med@hotmail.com>
2022-03-31 16:39:03 +0100

    setting token exp time to 24hrs

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -104,7 +104,7 @@ userController.login = (req, res, next) => {
             });
           }
           const token = jwt.sign({ userId: user._id }, key, {
-            expiresIn: 60,
+            expiresIn: 60 * 60 * 24,
           });
 
           res.status(200).json({
